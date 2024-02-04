f34e84256be052da242e3f88544ef646836bb99d
mahmoudGhunaim <141932536+mahmoudGhunaim@users.noreply.github.com>
2024-02-04 17:33:17 +0300

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./Header"
+import Header from "./header"
 import "./style/layout.css"
 import { Link } from "gatsby"
 const Layout = ({ children }) => {
